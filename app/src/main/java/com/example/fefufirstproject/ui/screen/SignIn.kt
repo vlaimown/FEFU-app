db4e3c0232a9f622eb7d2e262054672d0d1cf671
vlaimown <v1ad5tepanov@mail.ru>
2025-06-29 16:51:35 +1000

    Добавлено наполнение в SignIn. Добавлено AppNavigation

--- app/src/main/java/com/example/fefufirstproject/ui/screen/SignIn.kt
+++ app/src/main/java/com/example/fefufirstproject/ui/screen/SignIn.kt
@@ -1,2 +1,84 @@
 package com.example.fefufirstproject.ui.screen
 
+import androidx.navigation.NavController
+
+import androidx.compose.material3.Scaffold
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+
+import com.example.fefufirstproject.R
+import com.example.fefufirstproject.ui.widget.AppBar
+import com.example.fefufirstproject.ui.widget.BaseButton
+import com.example.fefufirstproject.ui.widget.BaseTextField
+import com.example.fefufirstproject.ui.widget.PasswordTextField
+
+@Composable
+fun SignIn(navController: NavController) {
+    Scaffold(
+        topBar = {
+            AppBar(title = stringResource(id = R.string.signin_screen_title)) {
+                navController.navigateUp()
+            }
+        },
+        content = { paddingValues ->
+            var login by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+
+            Column(
+                modifier = Modifier.fillMaxSize().padding(paddingValues).padding(dimensionResource(id = R.dimen.padding_small)),
+
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.spacer_small))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.welcome_screen_image),
+                    contentDescription = stringResource(
+                        id = R.string.image_description
+                    )
+                )
+
+                BaseTextField(
+                    value = login,
+                    onValueChange = { login = it },
+                    label = stringResource(id = R.string.login),
+                    validate = null
+                )
+
+                PasswordTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = stringResource(id = R.string.password),
+                    validate = null
+                )
+
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.spacer_small)))
+
+                BaseButton(
+                    true,
+                    text = stringResource(id = R.string.signin_screen_button),
+                    modifier = Modifier.fillMaxWidth().height(dimensionResource(id = R.dimen.button_height))
+                ) {
+                }
+            }
+        }
+    )
+}
